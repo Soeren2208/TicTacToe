8e8b84c4a187c46a0d7ecbc98d338140a33f9cf8
Sören Schwerk <s.schwerk@schule.bremen.de>
2022-10-05 09:17:55 +0200

    1. Benutzereingabe außerhalb des Boards Refactoring 2

--- src/test/java/de/szut/TicTacToeTest.java
+++ src/test/java/de/szut/TicTacToeTest.java
@@ -2,6 +2,8 @@ package de.szut;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -15,17 +17,11 @@ public class TicTacToeTest
         tictactoe = new TicTacToe();
     }
 
-    @Test
-    public void givenXOutsideBoard_WhenPlay_ThrowRunTimeException() {
+    @ParameterizedTest(name="x={0}, y={1}")
+    @CsvSource({"-1, -3", "4, 5", "-2, 3", "-2, 4", "0, 2", "4, 2", "1, 0", "2, -3", "2, 5"})
+    public void givenXOutsideBoard_WhenPlay_ThrowRunTimeException(int x, int y) {
         RuntimeException exception = assertThrows(RuntimeException.class,
-                () -> this.tictactoe.play(5, 2), "RuntimeException should have been thrown!");
-        assertEquals("Try is outside board!", exception.getMessage());
-    }
-
-    @Test
-    public void givenYOutsideBoard_WhenPlay_ThrowRunTimeException() {
-        RuntimeException exception = assertThrows(RuntimeException.class,
-                () -> this.tictactoe.play(2, 5), "RuntimeException should have been thrown!");
+                () -> this.tictactoe.play(x, y), "RuntimeException should have been thrown!");
         assertEquals("Try is outside board!", exception.getMessage());
     }
 
