e9e39e0f98049944b40ef3b8fe73c03233e70e2c
Sören Schwerk <s.schwerk@schule.bremen.de>
2022-10-05 08:59:47 +0200

    1. Benutzereingabe außerhalb des Boards

---
+++ src/test/java/de/szut/TicTacToeTest.java
@@ -0,0 +1,32 @@
+package de.szut;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class TicTacToeTest
+{
+    private TicTacToe tictactoe;
+
+    @BeforeEach
+    public void setUp(){
+        tictactoe = new TicTacToe();
+    }
+
+    @Test
+    public void givenXOutsideBoard_WhenPlay_ThrowRunTimeException() {
+        RuntimeException exception = assertThrows(RuntimeException.class,
+                () -> this.tictactoe.play(5, 2), "RuntimeException should have been thrown!");
+        assertEquals("X is outside board!", exception.getMessage());
+    }
+
+    @Test
+    public void givenYOutsideBoard_WhenPlay_ThrowRunTimeException() {
+        RuntimeException exception = assertThrows(RuntimeException.class,
+                () -> this.tictactoe.play(2, 5), "RuntimeException should have been thrown!");
+        assertEquals("Y is outside board!", exception.getMessage());
+    }
+
+}
